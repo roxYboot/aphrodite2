900b43346c4c2787c9abd3f48150ab85ea87f457
roxYboot <43090878+roxYboot@users.noreply.github.com>
2018-09-08 23:09:42 +0300

    Update aphrodite2.js

--- aphrodite2.js
+++ aphrodite2.js
@@ -13,7 +13,6 @@ client.on('ready', function() {
 });
 
       client.on('ready', () => {
-              client.user.setActivity("1play | .Fun", {type: 'LISTENING'});
       
       });
 /*
@@ -220,8 +219,7 @@ ${prefix}pause ⇏ إيقآف الأغنية مؤقتا
 ${prefix}resume ⇏ لموآصلة الإغنية بعد إيقآفهآ مؤقتا
 ${prefix}vol ⇏ لتغيير درجة الصوت 100 - 0
 ${prefix}stop ⇏ لإخرآج البوت من الروم
-${prefix}np ⇏ لمعرفة الأغنية المشغلة حآليا
-${prefix}queue ⇏ لمعرفة قآئمة التشغيل
+${prefix}join ⇏ لدخول البوت الروم
  `)
    message.channel.sendEmbed(embed)
     
